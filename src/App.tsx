152ad3d885e0b925e36bf736bd799fde1cd5de3d
Dravernuss <esteban16.rodas@gmail.com>
2025-01-31 21:03:13 -0500

    Create component MenuItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MenuItem from "./components/MenuItem";
 import { menuItems } from "./data/db";
 
 function App() {
@@ -8,6 +9,20 @@ function App() {
           Calculadora de Propinas y Consumo
         </h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
     </>
   );
 }
